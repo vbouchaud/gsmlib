2ed2c4e2fd2b4e21dcc05fe1fafa83a77eaa5574
Andrew Shadura <andrew@shadura.me>
2013-10-14 14:11:34 +0200

    Fix +CSMS response parsing: an argument can be zero-length (Closes: #602739).

--- gsmlib/gsm_me_ta.cc
+++ gsmlib/gsm_me_ta.cc
@@ -88,7 +88,17 @@ void MeTa::init() throw(GsmException)
 
   // find out whether we are supposed to send an acknowledgment
   Parser p(_at->chat("+CSMS?", "+CSMS:"));
-  _capabilities._sendAck = p.parseInt() >= 1;
+  try {
+    _capabilities._sendAck = p.parseInt() >= 1;
+  }
+  catch (GsmException &e)
+  {
+    if (e.getErrorClass() == ParserError) {
+      _capabilities._sendAck = 0;
+    } else {
+      throw e;
+    }
+  }
       
   // set GSM default character set
   try
@@ -1002,8 +1012,18 @@ void MeTa::setMessageService(int serviceLevel) throw(GsmException)
 
 unsigned int MeTa::getMessageService() throw(GsmException)
 {
-  Parser p(_at->chat("+CSMS?", "+CSMS:"));
-  return p.parseInt();
+  try {
+    Parser p(_at->chat("+CSMS?", "+CSMS:"));
+    return p.parseInt();
+  }
+  catch (GsmException &e)
+  {
+    if (e.getErrorClass() == ParserError) {
+      return 0;
+    } else {
+      throw e;
+    }
+  }
 }
 
 void MeTa::getSMSRoutingToTA(bool &smsRouted,
